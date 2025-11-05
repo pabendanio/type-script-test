bd73a3fb7774b7351bcb3dd577a18c3264ab560a
Cesar Paolo Abendanio <85096390+pabendanio@users.noreply.github.com>
2025-11-05 09:00:24 +0800

    Remove real webhook url (#4)

--- tests/setup.ts
+++ tests/setup.ts
@@ -7,7 +7,7 @@ process.env.DB_PORT = '5432';
 process.env.DB_USERNAME = 'postgresuser';
 process.env.DB_PASSWORD = 'postgrespassword';
 process.env.DB_NAME = 'birthday_app_test';
-process.env.WEBHOOK_URL = 'https://eolfa5dxoxzonv1.m.pipedream.net';
+process.env.WEBHOOK_URL = 'https://test-webhook.com';
 
 // Set timezone for consistent testing
 process.env.TZ = 'UTC';
